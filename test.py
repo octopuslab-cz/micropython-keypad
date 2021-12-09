371ef5e2f165b9efafdcc6da09e98843ddbc6579
Petr Kracík <petrkr@petrkr.net>
2021-12-09 21:03:25 +0100

    Test: use input method

--- test.py
+++ test.py
@@ -7,11 +7,7 @@ I2C_SDA = 22
 I2C_SCL = 21
 
 KP_ADDRESS = 0x25
-KP_DELAY = 250
 KP_INTERRUPT = 39
-
-lastKeyPress = 0
-keyDelay = KP_DELAY
 
 i2c = I2C(0, scl=Pin(I2C_SDA), sda=Pin(I2C_SCL), freq=400000)
 kp = I2CKeypad(i2c, KP_ADDRESS, 16)
@@ -19,19 +15,9 @@ kp = I2CKeypad(i2c, KP_ADDRESS, 16)
 displayNum = ""
 while True:
     try:
-        key = kp.getKey()
+        value = kp.input()
     except OSError as e:
         print("Error while get key")
         print(e)
-        key = None
 
-    if key and ticks_ms() > lastKeyPress+keyDelay:
-        lastKeyPress = ticks_ms()
-        print(key)
-        if key == '#': # Enter
-            print("final number: ", displayNum)
-        else:    
-           displayNum += str(key)
-
-        if key == 'C': # Clear
-           displayNum = ""
+    print(value)
