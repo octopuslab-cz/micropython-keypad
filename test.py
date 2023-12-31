6d18f734616e7fba4d57a51380eede485975312e
kaamburoow <132601893+kaamburoow@users.noreply.github.com>
2023-12-31 23:47:02 +0200

    Fix TypeError: extra keyword arguments given

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ I2C_SCL = 21
 KP_ADDRESS = 0x25
 KP_INTERRUPT = 39
 
-i2c = I2C(0, scl=Pin(I2C_SDA), sda=Pin(I2C_SCL), freq=400000)
+i2c = I2C(scl=Pin(I2C_SDA), sda=Pin(I2C_SCL), freq=400000)
 kp = I2CKeypad(i2c, KP_ADDRESS, 16)
 
 def on_keypress(key):
